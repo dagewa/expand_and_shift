f839ef8d313eb38168216feb9d7829ef2741f330
David Waterman <dgwaterman@gmail.com>
2025-08-05 17:29:20 +0100

    Only install with "python" if it is a DIALS python

--- install.sh
+++ install.sh
@@ -5,5 +5,11 @@ SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 if command -v libtbx.python >/dev/null 2>&1; then
     exec libtbx.python "${SCRIPT_DIR}/install.py"
 else
-    exec python "${SCRIPT_DIR}/install.py"
+    # Check if plain "python" is a DIALS installation
+    if python -c "from dials import util"; then
+        exec python "${SCRIPT_DIR}/install.py"
+    else
+        echo "A DIALS installation is required."
+        exit 1
+    fi
 fi
